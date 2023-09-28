be5179723b0b0e990bb902d00a4cfe9f426061ab
Prabhdip Gill <pg53@rice.edu>
2023-09-28 11:37:48 -0700

    Update code and test to ignore unexpected columns

--- loaders/course_content_loader.py
+++ loaders/course_content_loader.py
@@ -53,19 +53,22 @@ def main():
 
     with session_factory.begin() as session:
         for eachRecord in records:
-            visible = True if eachRecord['visible'] == '1' else False
+            visible = eachRecord['visible'] == '1'
             eachRecord['visible'] = visible
             eachRecord['term'] = term
-            insert_stmt = insert(CourseContent).values(**eachRecord)
+
+            record_data = {
+                'section': eachRecord['section'],
+                'activity_name': eachRecord['activity_name'],
+                'lesson_page': eachRecord['lesson_page'],
+                'visible': visible,
+                'updated_at': datetime.utcnow(),
+            }
+
+            insert_stmt = insert(CourseContent).values(**record_data)
             do_update_stmt = insert_stmt.on_conflict_do_update(
                 index_elements=['term', 'content_id'],
-                set_=dict(
-                    section=eachRecord['section'],
-                    activity_name=eachRecord['activity_name'],
-                    lesson_page=eachRecord['lesson_page'],
-                    visible=visible,
-                    updated_at=datetime.utcnow()
-                )
+                set_=record_data  # Use the filtered dictionary here
             )
             session.execute(do_update_stmt)
 
